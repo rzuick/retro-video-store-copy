669e8f7cdae565adb84bdd838978a3c2b781129d
Rebecca Z <80494343+rzuick@users.noreply.github.com>
2021-11-05 23:10:38 -0500

    created helper functions to test for valid/invalid data and customer

--- app/routes/customer_routes.py
+++ app/routes/customer_routes.py
@@ -1,9 +1,9 @@
-# import stuff
 from app import db
 from flask import Blueprint, jsonify, request
 from app.models.customer import Customer
 
 customer_bp = Blueprint("", __name__, url_prefix="/customers")
+
 
 def invalid_data(request_body):
     if "name" not in request_body:
@@ -15,34 +15,73 @@ def invalid_data(request_body):
     else:
         valid = False
     return valid
-    
-@customer_bp.route("", methods = ["POST"])
+
+
+def invalid_customer(customer_id):
+    invalid = False
+    if not customer_id.isnumeric():
+        invalid = {"message": f"Invalid customer id"}, 400
+    elif customer_id.isnumeric():
+        if Customer.query.get(customer_id) is None:
+            invalid = {"message": f"Customer {customer_id} was not found"}, 404
+    return invalid
+
+
+@customer_bp.route("", methods=["POST"])
 def post_customer():
     request_body = request.get_json()
     invalid_response = invalid_data(request_body)
     if not invalid_response:
         new_customer = Customer(
-            name= request_body["name"],
-            postal_code= request_body["postal_code"],
-            phone= request_body["phone"] 
+            name=request_body["name"],
+            postal_code=request_body["postal_code"],
+            phone=request_body["phone"]
         )
         db.session.add(new_customer)
         db.session.commit()
         return jsonify(new_customer.to_json()), 201
     return jsonify(invalid_response), 400
 
-@customer_bp.route("", methods = ["GET"])
+
+@customer_bp.route("", methods=["GET"])
 def get_customers():
     customers = Customer.query.all()
     customer_list = [customer.to_json() for customer in customers]
     return jsonify(customer_list), 200
 
-@customer_bp.route("/<customer_id>", methods = ["GET"])
+
+@customer_bp.route("/<customer_id>", methods=["GET"])
 def get_one_customer(customer_id):
-    if customer_id.isnumeric():
-        one_customer = Customer.query.get(customer_id)
-        if one_customer is None:
-            return jsonify({"message": f"Customer {customer_id} was not found"}), 404
-        else:
-            return jsonify(one_customer.to_json()), 200
-    return jsonify({"message": f"Invalid customer id"}), 400
+    invalid_response = invalid_customer(customer_id)
+    if invalid_response:
+        return invalid_response
+    one_customer = Customer.query.get(customer_id)
+    return jsonify(one_customer.to_json()), 200
+
+
+@customer_bp.route("/<customer_id>", methods=["PUT"])
+def update_customer(customer_id):
+    one_customer = Customer.query.get(customer_id)
+    invalid_cust = invalid_customer(customer_id)
+    if invalid_cust:
+        return invalid_cust
+    request_body = request.get_json()
+    invalid_response = invalid_data(request_body)
+    if invalid_response:
+        return jsonify(invalid_response), 400
+    one_customer.name = request_body["name"]
+    one_customer.phone = request_body["phone"]
+    one_customer.postal_code = request_body["postal_code"]
+    db.session.commit()
+    return jsonify(one_customer.to_json()), 200
+
+
+@customer_bp.route("/<customer_id>", methods=["DELETE"])
+def delete_customer(customer_id):
+    one_customer = Customer.query.get(customer_id)
+    invalid_cust = invalid_customer(customer_id)
+    if invalid_cust:
+        return invalid_cust
+    db.session.delete(one_customer)
+    db.session.commit()
+    return jsonify(one_customer.to_json()), 200
